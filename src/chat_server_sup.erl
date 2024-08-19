5ce4a5a4cdfef8cccedc7877eb88bcae05c2e198
zacharyabolton <zacbolton2129@gmail.com>
2024-08-18 22:20:55 -0300

    More explicit master supervisor syntax.
    Refactor init function to use maps for clarity and explicit supervision
    strategy definitions.

--- src/chat_server_sup.erl
+++ src/chat_server_sup.erl
@@ -13,23 +13,13 @@ start_link() ->
 	supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
-  Children = [
-    % Keep alive process to prevent the container from stopping
-    {keep_alive, {keep_alive, start_link, []},
-     permanent, 5000, worker, [keep_alive]}
-  ],
-
-  % One_for_one strategy restarts only the crashed process
-  {ok, {{one_for_one, 10, 10}, Children}}.
-
-% init([]) ->
-%     SupFlags = #{strategy => one_for_one,
-%                  intensity => 10,
-%                  period => 10},
-%     ChildSpecs = [#{id => keep_alive,
-%                     start => {keep_alive, start_link, []},
-%                     restart => permanent,
-%                     shutdown => 5000,
-%                     type => worker,
-%                     modules => [keep_alive]}],
-%     {ok, {SupFlags, ChildSpecs}}.
+    SupFlags = #{strategy => one_for_one,
+                 intensity => 10,
+                 period => 10},
+    ChildSpecs = [#{id => keep_alive,
+                    start => {keep_alive, start_link, []},
+                    restart => permanent,
+                    shutdown => 5000,
+                    type => worker,
+                    modules => [keep_alive]}],
+    {ok, {SupFlags, ChildSpecs}}.
