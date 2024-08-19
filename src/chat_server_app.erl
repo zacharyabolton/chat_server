248e9ff4432e067b19c44867cd35157afc33a766
zacharyabolton <zacbolton2129@gmail.com>
2024-08-18 22:18:31 -0300

    Adds logging to top level app module.

--- src/chat_server_app.erl
+++ src/chat_server_app.erl
@@ -14,14 +14,26 @@
 
 start(_Type, _Args) ->
     io:format("Starting chat_server_app~n"),
+    {ok, _} = application:ensure_all_started(cowboy),
+    io:format("Cowboy started~n"),
     Dispatch = cowboy_router:compile([
         {'_', [{"/", hello_handler, []}]}
     ]),
-    {ok, _} = cowboy:start_clear(my_http_listener,
+    io:format("Dispatch compiled~n"),
+    case cowboy:start_clear(my_http_listener,
         [{port, 8080}],
         #{env => #{dispatch => Dispatch}}
-    ),
-    chat_server_sup:start_link().
+    ) of
+        {ok, _} ->
+            io:format("Cowboy HTTP listener started on port 8080~n");
+        Error ->
+            io:format("Failed to start Cowboy HTTP listener: ~p~n", [Error])
+    end,
+    Result = chat_server_sup:start_link(),
+    io:format("chat_server_sup:start_link() result: ~p~n", [Result]),
+    Result.
 
 stop(_State) ->
-	ok.
+    io:format("Stopping chat_server_app~n"),
+    ok = cowboy:stop_listener(my_http_listener),
+    ok.
