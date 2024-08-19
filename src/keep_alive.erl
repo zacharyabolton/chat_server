3f761d92f14351b87a5573b087c4b610f44ddcb4
zacharyabolton <zacbolton2129@gmail.com>
2024-08-18 22:05:28 -0300

    Returns Pid from start_link.

--- src/keep_alive.erl
+++ src/keep_alive.erl
@@ -5,10 +5,11 @@
 -export([start_link/0]).
 
 start_link() ->
-    keep_alive().
+    Pid = spawn_link(fun() -> keep_alive_loop() end),
+    {ok, Pid}.
 
-keep_alive() ->
-  io:format("Keep alive sleeping on node ~p~n", [node()]),
+keep_alive_loop() ->
+    io:format("Keep alive sleeping on node ~p~n", [node()]),
   %% Sleep for 2 seconds
-  timer:sleep(2000),
-  keep_alive().
+    timer:sleep(2000),
+    keep_alive_loop().
