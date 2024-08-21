c8d4b82f169388e087c09043d40b550e7212676f
zacharyabolton <zacbolton2129@gmail.com>
2024-08-20 22:10:43 -0300

    Re-pushing passing base tests.

--- test/keep_alive_tests.erl
+++ test/keep_alive_tests.erl
@@ -9,7 +9,6 @@
 %% Test cases
 
 keep_alive_start_test() ->
-  % {ok, _Pid} = keep_alive:start_link(),
-  % % Assert that the process is alive
-  % ?assert(is_pid(_Pid)).
-  ?assert(false).
+  {ok, _Pid} = keep_alive:start_link(),
+  % Assert that the process is alive
+  ?assert(is_pid(_Pid)).
